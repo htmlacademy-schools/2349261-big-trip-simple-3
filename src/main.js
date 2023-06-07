c181874c7460862fd22a1d03aaa560045055e5cc
daniil <dankras13@mail.ru>
2023-06-07 05:07:24 +0300

    module8-task1

--- src/main.js
+++ src/main.js
@@ -1,28 +1,59 @@
-import Filters from './view/filters.js';
 import BoardPresenter from './presenter/board-presenter';
 import ModelWaypoint from './model/model-waypoint';
-import {mockInit, waypoints} from './mock/point';
-import {render} from './framework/render';
-import ModelOffers from './model/model-offers.js';
-import ModelDestinations from './model/model-destinations.js';
-import {offersByType} from './mock/const';
-import {destinations} from './mock/destination';
+import ModelOffers from './model/model-offers';
+import ModelDestinations from './model/model-destinations';
+import ModelFilters from './model/model-filter';
+import FilterPresenter from './presenter/filter-presenter';
+import {render} from './render';
+import NewWaypointButton from './view/new-waypoint-button';
+import WaypointsApiService from './waypoints-api-service';
 
 const siteHeaderElement = document.querySelector('.trip-controls__filters');
 const container = document.querySelector('.trip-events');
+const placeForButton = document.querySelector('.trip-main');
 
-mockInit(3, 10);
-const modelWaypoints = new ModelWaypoint(waypoints);
-const modelOffers = new ModelOffers(offersByType);
-const modelDestinations = new ModelDestinations(destinations);
+const AUTHORIZATION = 'Basic sgkdajgskdgas7777';
+const END_POINT = 'https://18.ecmascript.pages.academy/big-trip';
+
+const waypointsApiService = new WaypointsApiService(END_POINT, AUTHORIZATION);
+
+const modelWaypoints = new ModelWaypoint({waypointsApiService});
+const modelOffers = new ModelOffers({waypointsApiService});
+const modelDestinations = new ModelDestinations({waypointsApiService});
+const modelFilter = new ModelFilters();
 
 const boardPresenter = new BoardPresenter({
   boardContainer: container,
   waypointsModel: modelWaypoints,
   modelOffers,
-  modelDestinations
+  modelDestinations,
+  modelFilter,
+  onNewWaypointDestroy: handleNewTaskFormClose
 });
 
-render(new Filters(), siteHeaderElement);
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteHeaderElement,
+  modelFilter,
+  modelWaypoints
+});
 
+const newWaypointButtonComponent = new NewWaypointButton({
+  onClick: handleNewTaskButtonClick
+});
+
+function handleNewTaskFormClose() {
+  newWaypointButtonComponent.element.disabled = false;
+}
+
+function handleNewTaskButtonClick() {
+  boardPresenter.createWaypoint();
+  newWaypointButtonComponent.element.disabled = true;
+}
+
+filterPresenter.init();
 boardPresenter.init();
+modelWaypoints.init()
+  .finally(() => {
+    render(newWaypointButtonComponent, placeForButton);
+  });
+
